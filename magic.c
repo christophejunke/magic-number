1beac1ceb45344785723f21caae90baac5eeb033
christophe <christophe.junke@trust-in-soft.com>
2017-09-14 15:57:12 +0200

    Change comments

--- magic.c
+++ magic.c
@@ -1,18 +1,21 @@
 void magic_number(void)
 {
-        /* Short value tmp is 0x5511, which is stored as 2 consecutive bytes.
-           Difference with big or little endian architectures:
+        /* Short value tmp is 0x5511, which can be manipulated as 2
+           consecutive bytes:
 
-              Little endian : 0x11 0x55
+              Byte index:     0    1
+             ---------------------------
               Big endian    : 0x55 0x11
+              Little endian : 0x11 0x55
+
          */
         short tmp = (short)(0x5511);
 
         /* Convert to pointer to char* (char is 1 byte). */
         char* b = ((char *)&tmp) ;
 
-        /* Take the first char.
-           Depending on the architecture, c is either 0x55 or 0x11.
+        /* Take the first char. Depending on the architecture, c is
+         * either 0x55 or 0x11.
         */
         char c = b[0];
 
